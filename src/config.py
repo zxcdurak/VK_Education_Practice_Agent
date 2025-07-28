cca61450503c8ab7ba28a428390fdadd4293931b
Admin <Admin@Y3_MAX>
2025-07-28 22:32:28 +0700

    update config.py

--- src/config.py
+++ src/config.py
@@ -1,12 +1,12 @@
-from pydantic import ConfigDict
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     vk_token: str
     giga_auth_key: str
     clientSecret: str
     clientID: str
-    
-    model_config = ConfigDict(env_file=".env")
+    model_LLM: str
+
+    model_config = SettingsConfigDict(env_file="../.env")
 
 settings = Settings()
