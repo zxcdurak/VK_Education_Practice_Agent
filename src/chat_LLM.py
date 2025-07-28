c07c71cf4792c926f0ffea1752099ebbae1fdb29
Admin <Admin@Y3_MAX>
2025-07-28 23:29:25 +0700

    redesigned interaction with llm

---
+++ src/chat_LLM.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_gigachat.chat_models import GigaChat
+from config import settings
+from PromptManager import manager
+
+
+class AbstractChat(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def ask(self, question: str) -> str:
+        pass
+
+
+class GigaChatImpl(AbstractChat):
+    def __init__(self, model: str):
+        super().__init__()
+        self.giga = GigaChat(
+            credentials=settings.giga_auth_key,
+            verify_ssl_certs=False,
+            model=model
+        )
+
+    def ask(self, question: str) -> str:
+        messages = [
+            SystemMessage(
+                content=manager.get_prompt("vk_agent")
+            )]
+        messages.append(HumanMessage(content=question))
+        res = self.giga.invoke(messages)
+        return res.content
+
+
+gigachat = GigaChatImpl(settings.model_LLM)
