b5f8941d3c39e4477150fd0d93280e036f14b059
Admin <Admin@Y3_MAX>
2025-07-29 12:39:40 +0700

    style PromptManager.py

--- src/PromptManager.py
+++ src/PromptManager.py
@@ -1,10 +1,8 @@
 class PromptManager:
-    path: str
-
     def __init__(self, path: str):
         self.path = path
 
-    def get_prompt(self, name: str) -> str:
+    def get_prompt(self, name: str) -> str | None:
         try:
             with open(f"{self.path}/{name}" + ".txt") as f:
                 return f.read()
