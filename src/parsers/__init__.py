43f6caf9732d5c731a44a40653708dd7178270db
Admin <Admin@Y3_MAX>
2025-07-29 21:29:26 +0700

    create module parsers

---
+++ src/parsers/__init__.py
@@ -0,0 +1,5 @@
+from .faq_parser import FAQParser
+from .programs_parser import ProgramsParser
+
+
+__all__ = ['FAQParser', 'ProgramsParser']
--- src/parsers/programs_parser.py
+++ src/parsers/programs_parser.py
@@ -1,44 +1,7 @@
 import requests
-from bs4 import BeautifulSoup
 import orjson
-import re
+from bs4 import BeautifulSoup
 from pathlib import Path
-
-from test import clean_program_text
-
-class FAQParser:
-    def __init__(self, url: str):
-        self.url = url
-        self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-        }
-
-    def fetch_content(self):
-        """Получает содержимое страницы."""
-        response = requests.get(self.url, headers=self.headers)
-        response.raise_for_status()
-        return response.content
-
-    def extract_text_blocks(self) -> str:
-        """Извлекает текст из блоков .tn-atom."""
-        content = self.fetch_content()
-        soup = BeautifulSoup(content, "html.parser")
-        divs = "\n".join(
-            [i.text.replace("\u2028", " ") for i in soup.select('.tn-atom')]
-        )
-        return divs
-
-    def extract_faq(self) -> str:
-        """Извлекает пары вопрос-ответ из .t668__header и .t668__content."""
-        content = self.fetch_content()
-        soup = BeautifulSoup(content, "html.parser")
-        FAQ_q = soup.select(".t668__header")
-        FAQ_a = soup.select(".t668__content")
-        faq_text = "".join([
-            f"{FAQ_q[i].text.strip()}:{FAQ_a[i].text.strip()}\n"
-            for i in range(min(len(FAQ_q), len(FAQ_a)))
-        ])
-        return faq_text
 
 
 class ProgramsParser:
@@ -108,18 +71,11 @@ class ProgramsParser:
         print(f"Parsed programs saved to {filepath}")
 
 
-
-
-faq_parser = FAQParser(url="https://tilda-embed.tech-mail.ru/redmine_issue_13800")
-
-
-# Парсинг программ со второй страницы
 progs_parser = ProgramsParser(
     url="https://store.tildaapi.com/api/getproductslist/?storepartuid=357127554781&recid=754421136&c=1752668309883&sort%5Bcreated%5D=desc&size=100",
     dir="src/parsed_data/"
     )
 
-# Сохранение данных
 progs_parser.parse_programs()
 progs_parser.save_raw_data("data_pretty.json")
 progs_parser.save_parsed_data("data_parsed.json")
